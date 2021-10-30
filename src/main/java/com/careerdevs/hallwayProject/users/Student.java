304f1bdf68908ef91df94a579f17aa3636718069
Travis J <tfauvelle@gmail.com>
2021-10-30 10:07:14 -0400

    abstract user and student defined.

--- src/main/java/com/careerdevs/hallwayProject/users/Student.java
+++ src/main/java/com/careerdevs/hallwayProject/users/Student.java
@@ -4,8 +4,34 @@ package com.careerdevs.hallwayProject.users;
 import javax.persistence.Entity;
 
 @Entity
-public class Student {
-    private long Id;
+public class Student extends User {
+//    private Locker locker;
 
 
+    public Student(){}
+
+    public Student(long id, String firstName, String lastName, String userName) {
+        super(id, firstName, lastName, userName);
+    }
+
+
+    @Override
+    void goToLocker() {
+
+    }
+
+    @Override
+    void putInLocker() {
+
+    }
+
+    @Override
+    void updateLocker() {
+
+    }
+
+    @Override
+    void deleteFromLocker() {
+
+    }
 }
--- src/main/java/com/careerdevs/hallwayProject/users/User.java
+++ src/main/java/com/careerdevs/hallwayProject/users/User.java
@@ -1,14 +1,55 @@
 package com.careerdevs.hallwayProject.users;
 
 public abstract class User {
-    private long Id;
+    private long id;
     private String firstName;
     private String lastName;
     private String userName;
+
+
+    public User() {}
+
+    public User(long id, String firstName, String lastName, String userName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+    }
 
     abstract void goToLocker();
     abstract void putInLocker();
     abstract void updateLocker();
     abstract void deleteFromLocker();
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
