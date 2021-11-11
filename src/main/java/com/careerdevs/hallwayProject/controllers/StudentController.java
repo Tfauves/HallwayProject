16f34354a8cbd5bfd56cc7e74ee3dea8e1b582bc
Tfauves <tfauvelle@gmail.com>
2021-11-11 10:12:14 -0500

    GetMappings.

--- src/main/java/com/careerdevs/hallwayProject/controllers/StudentController.java
+++ src/main/java/com/careerdevs/hallwayProject/controllers/StudentController.java
@@ -1,4 +1,38 @@
 package com.careerdevs.hallwayProject.controllers;
 
+
+import com.careerdevs.hallwayProject.models.Student;
+import com.careerdevs.hallwayProject.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
+
+    @Autowired
+    private StudentRepository repository;
+
+    @GetMapping
+    public @ResponseBody List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Student findById(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Student>> findByCohort(@PathVariable Integer cohort) {
+        return new ResponseEntity<>(repository.findByCohort(cohort, Sort.by("lastName")), HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/careerdevs/hallwayProject/repositories/StudentRepository.java
+++ src/main/java/com/careerdevs/hallwayProject/repositories/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.careerdevs.hallwayProject.repositories;
 
 import com.careerdevs.hallwayProject.models.Student;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByCohort(Integer cohort, Sort sort);
 }
